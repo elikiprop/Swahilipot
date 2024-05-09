ce114bcd38700d2862298a15c81cc04c26f7507c
Eli Kiprop <elikiprop42@gmail.com>
2024-05-09 11:40:51 +0300

    update ui

--- index.py
+++ index.py
@@ -1,13 +1,13 @@
 a = 10
 b = 5
-print(f"First: {a, b}")
+print(f"First: ({a}, {b})")
 
 """
 First: (10, 5)
 """
 
 a, b = b, a + 2
-print(f"Second: {a, b}")
+print(f"Second: ({a}, {b})")
 
 """
 Second: (5, 12)
